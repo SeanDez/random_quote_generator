a805e975b608efbc8614f99897a47e8fddb45269
Sean Dez <sd@seandezoysa.com>
2018-10-22 03:02:19 +0700

    add quote retreival

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// request quote.text and author
+
+
+
+const fetch_new_quote = function () {
+    const xhr = new XMLHttpRequest();
+    const quote_api_url = 'https://talaikis.com/api/quotes/random/';
+
+    xhr.open('GET', quote_api_url);
+    xhr.send();
+
+
+    // a property that contains the event handler that executes when the readystatechange event is fired
+    // can't just use the if block. This handler opens a watcher
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            update_texts(xhr);
+        }
+    }
+};
+
+const update_texts = function (response_object) {
+    console.log(response_object.responseText);
+
+    // parse the response property string into a JS object
+    const parsed_response_text = JSON.parse(response_object.responseText);
+
+
+    // grab an element
+    const quote_element = document.getElementById("text");
+    const author_element = document.getElementById("author");
+
+    // set the elements equal to the new text
+    quote_element.innerText = parsed_response_text.quote;
+    author_element.innerText = parsed_response_text.author;
+
+    console.log(parsed_response_text);
+    console.log(parsed_response_text.quote);
+
+};
+
+// link the function to the get quote button in a separated way
+
+document.getElementById("new-quote").addEventListener("click", fetch_new_quote);
+
+
+// setup the quote fetch on load
+
+window.onload = fetch_new_quote;
